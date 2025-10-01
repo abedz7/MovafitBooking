b7549ad3fd2a32f3e60ec732ab90b2d77f96b65f
Abed jaber <abdo.jaber.123@gmail.com>
2025-10-01 22:13:09 +0300

    s
    s

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="login-container">
+        <img src={require('./assets/movfit.png')} alt="Movafit Logo" className="logo" />
+        <h2>Admin Login</h2>
+        <form className="login-form">
+          <input type="text" placeholder="Phone Number" />
+          <input type="password" placeholder="Password" />
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </div>
   );
 }
